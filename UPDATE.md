2b942d0705fbcd1f64a8d936a194b5a6b6b4474e
aestriga <aestriga@lasige.di.fc.ul.pt>
2021-01-04 15:08:48 +0000

    added update-md

---
+++ UPDATE.md
@@ -0,0 +1,9 @@
+# qualweb-extension
+## How to update
+
+Update versions in package-json ("@qualweb/qw-page","@qualweb/act-rules","@qualweb/wcag-techniques")
+
+```shell
+  $ npm i
+  $ npm run build
+```
--- src/content.js
+++ src/content.js
@@ -10,11 +10,11 @@ function evaluateACT() {
     let act, actResult, result;
     act = new ACTRules.ACTRules();
     let start = Date.now();
-    console.log("Starting evaluation" + start);
+    //console.log("Starting evaluation" + start);
     actResult = act.execute([], currentPage, []);
-    console.log("Ending evaluation act" + Math.floor((start - Date.now()) / 1000));
+    //console.log("Ending evaluation act" + Math.floor((start - Date.now()) / 1000));
     addValuesToSummary(summary, actResult);
-    console.log(summary);
+    //console.log(summary);
     result = actResult.assertions;
     return result;
 }
@@ -23,10 +23,10 @@ function evaluateWCAG() {
     let html, htmlResult, result;
     html = new WCAGTechniques.WCAGTechniques();
     let start = Date.now();
-    console.log("Starting evaluation" + start);
+   // console.log("Starting evaluation" + start);
     htmlResult = html.execute(currentPage, false, {});
-    console.log("Ending evaluation WCAG" + Math.floor((start - Date.now()) / 1000));
-    console.log(htmlResult);
+   // console.log("Ending evaluation WCAG" + Math.floor((start - Date.now()) / 1000));
+   // console.log(htmlResult);
     addValuesToSummary(summary, htmlResult);
     result = htmlResult.assertions;
     return result;
